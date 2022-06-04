e8476d13e4ee4a4d276d12cbe7b659aed1cde73b
bitia <bitia_r_24@hotmail.com>
2022-06-04 16:37:18 +0200

    #1 Navbar

---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { Box, AppBar,Toolbar, InputBase, Avatar, Stack } from 
+'@mui/material';
+import logo3 from '../imagenes/logo3.png';
+import SearchIcon from '@mui/icons-material/Search';
+import gorila3 from '../imagenes/gorila3.png';
+import cesta from '../imagenes/cesta.png';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color:'primary',
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: '#fffafa',
+  height:'15vh',
+}));
+
+const LogoBoxStyled = styled(Box)(({ theme }) => ({
+ 
+   
+}));
+function NavBar() {
+    return (
+      <Box sx={{ flexGrow: 1 }} >
+        <StyledAppBar position="static" elevation={0} >
+          <Toolbar variant="dense" sx={{ display: 'flex', justifyContent: 
+  'space-around' }}>
+            <LogoBoxStyled>
+            <img src={logo3} alt='logo' sx={{width: 100, height: 100}}/>
+            </LogoBoxStyled>
+  
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Stack direction="row" spacing={3}>
+              <Avatar
+                alt="gorila" src={gorila3} sx={{ width: 70, height: 70 }} />
+              <Avatar
+              alt="cesta" src={cesta} sx={{ width: 70, height: 70 }}/>
+              </Stack>
+            
+         </Toolbar>
+        </StyledAppBar>
+      </Box>
+    );
+  }
+  export default NavBar
