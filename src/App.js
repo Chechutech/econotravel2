9ee3cb62b543e7452328e15aec8412629f562ae3
bitia <bitia_r_24@hotmail.com>
2022-06-07 08:48:13 +0200

    #2 cambios en Catalogo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Footer from './componentes/Footer/Footer';
 import NavBar from './componentes/NavBar/NavBar';
+import Catalogo from './componentes/Catalogo/Catalogo';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-     
+      <Catalogo/>
       <Footer/>
     </div>
   );
