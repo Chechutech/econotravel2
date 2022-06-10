4e716947ea0b3d71796cfd356c45f898fd78fa9a
Sandra Martos <bysmartos@gmail.com>
2022-06-10 06:39:06 +0200

    #14 parte de exp detalladas

--- src/componentes/Home/ExpDestacadas.jsx
+++ src/componentes/Home/ExpDestacadas.jsx
@@ -8,23 +8,35 @@ const Item2 = styled(Paper)(({ theme }) => ({
     color: "#4B7F55",
   }));
 
+  const StyledContainer = styled(Container)(({ theme }) => ({
+    paddingTop: theme.spacing(30),
+    paddingBottom: theme.spacing(6),
+    maxWidth: "lg",
+    backgroundColor: "#FFFAFA",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent:"space-around",
+  }));
+
 const ExpDestacadas = () => {
     const array= data.slice(0,3)
     console.log(array)
     return (  
     <>
-    <Container sx={{display:'flex'}}> 
+    <StyledContainer > 
     
-          <Grid  container spacing={4} >
+          <Grid  container spacing={14} >
           {array.map((exp, index) => (
               <Grid key={index} item xs={12} sm={6} md={4}>
                   <Card  sx={{ maxWidth: 285}} >
                        <CardMedia
                               component="img"
-                              height="381"
+                              height="370"
                               alt="bici montaña"
                               image={exp.img} />
                    <CardContent>
+                   <Typography variant="body1" style={{fontWeight:"bold", color:"#4B7F55" }}>{exp.titulo}</Typography>  
+
                <Stack direction="row" spacing={1}>
                    <Item2>Chip One1</Item2>
                     <Item2>Chip One</Item2>
@@ -33,7 +45,7 @@ const ExpDestacadas = () => {
                 <CardActions style={{justifyContent:"space-between"}} >
                   <Typography style={{fontSize:"0.8rem", color:"#4B7F55", fontWeight:"bold"}} >{exp.etiquetas[0].ubicacion}
                   <Typography style={{fontSize:"0.7rem", color:"#2F2F2F"}} >
-                  {exp.precio}
+                  {exp.precio}€ Por persona
                     </Typography>
                   </Typography>
                 <Button style={{fontSize:"0.6rem", color:"#2F2F2F", fontWeight:"bold"}}>Reserva ahora</Button>
@@ -43,7 +55,7 @@ const ExpDestacadas = () => {
                 </Grid>
                 ))}
            </Grid>
-           </Container>
+           </StyledContainer>
     </> );
 }
  
