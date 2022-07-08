ddf093343dd0605f9e980cc283f71ddc9f3ff046
bitia <bitia_r_24@hotmail.com>
2022-07-08 09:52:05 +0200

    #1 corrigiendo errores

--- server2/server.ts
+++ server2/server.ts
@@ -1,20 +1,36 @@
-import dotenv from 'dotenv'
-import router from './route/expRoute'
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import  router  from './route/expRoute';
+import cors from 'cors';
+import dotenv from 'dotenv'; 
+//const experienciaLista = require('./controller/getExperiencias')
 
-dotenv.config();
+ dotenv.config();
 
-// pools will use environment variables
-// for connection information
 
-const app = express()
-const port = process.env.PORT || 8000
+
+const app = express();
+
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use( router);
+
 
 app.listen(port, () => {
-    console.log('listening on http://localhost:${port}');
-})
+    console.log(`listening on http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
