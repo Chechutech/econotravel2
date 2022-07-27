b0e06459577b352af8ec4df905c47fe35d0276bd
chechu <ceciaolmedo@gmail.com>
2022-07-27 18:34:46 +0200

    authentication/login

--- server2/src/middleware/authjwt.ts
+++ server2/src/middleware/authjwt.ts
@@ -1,5 +1,5 @@
-/*import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv'; 
+import jwt from 'jsonwebtoken';
+/*import dotenv from 'dotenv'; 
 
 dotenv.config();
 
@@ -8,3 +8,34 @@ export const tokenGenerator (User) {
         user:
     }
 }*/
+import { Request, Response} from "express";
+import { NextFunction } from "express";
+
+
+export const authMiddleware = async (req:Request, res: Response, next: NextFunction)=>{
+    if (verifyToken(req)) { next()} else{
+        res.send("you are not authenticated")
+    }
+
+}
+
+export const verifyToken = (req:Request) => {
+    const authorization = req.get('authorization');
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+
+        let token= authorization.substring(7)
+        try {
+            jwt.verify(token, process.env.JWT_SECRET!);
+        return true;} 
+        catch { return false;
+
+        }
+
+    } else {
+        
+        return false;
+    
+    }
+}
+
